327582ccdf6199ae3fdcb2ce37658a0a0215c542
anthoni portocarrero rodriguez <softwaretoni21@gmail.com>
2025-06-11 23:39:50 -0500

    feat: clean up Mongoose connection options in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,14 @@ import { UsersModule } from './users/users.module';
 import { envs } from './config/envs';
 import { ViewsModule } from './views/views.module';
 import { RolesModule } from './roles/roles.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRoot(envs.MONGODB_URI, {
-      // Opciones de conexión
       maxPoolSize: 10,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
-      bufferMaxEntries: 0,
       bufferCommands: false,
     }),
     UsersModule,
